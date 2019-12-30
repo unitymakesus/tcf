3147950074e4d62f191683768d6f5db7a85590b8
Andrew Marino <andrew@andrewmarino.net>
2019-12-29 21:38:08 -0500

    setup local acf-json for child theme, commit field groups

--- wp-content/themes/unity-child/app/admin.php
+++ wp-content/themes/unity-child/app/admin.php
@@ -3,6 +3,41 @@
 namespace App;
 
 /**
+ * ACF Local JSON
+ * @source https://www.advancedcustomfields.com/resources/local-json/
+ */
+add_filter('acf/settings/save_json', function() {
+  return get_stylesheet_directory() . '/acf-json';
+});
+
+add_filter('acf/settings/load_json', function($paths) {
+  if (is_child_theme()) {
+    $paths[] = get_template_directory() . '/acf-json';
+  }
+
+  return $paths;
+});
+
+/**
+ * ACF Theme Options
+ */
+if ( function_exists('acf_add_options_page') ) {
+  acf_add_options_page([
+    'page_title' 	=> 'Theme General Settings',
+    'menu_title'	=> 'Theme Settings',
+    'menu_slug' 	=> 'theme-general-settings',
+    'capability'	=> 'edit_posts',
+    'redirect'		=> false,
+  ]);
+
+  acf_add_options_sub_page([
+    'page_title' 	=> 'County Map Settings',
+    'menu_title'	=> 'County Map',
+    'parent_slug'	=> 'theme-general-settings',
+  ]);
+}
+
+/**
  * Templates and Page IDs without editor
  */
 function ea_disable_editor( $id = false ) {
