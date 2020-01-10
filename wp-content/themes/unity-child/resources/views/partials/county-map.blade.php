933be2ab185bd2851ebb1fe36c292fbb0567a569
Andrew Marino <andrew@andrewmarino.net>
2020-01-10 14:38:54 -0500

    revise county map for mobile, add number formatter function

--- wp-content/themes/unity-child/resources/views/partials/county-map.blade.php
+++ wp-content/themes/unity-child/resources/views/partials/county-map.blade.php
@@ -1,21 +1,30 @@
 @php
-
-$all = get_field('county_map_all_county', 'option');
-$chatham = get_field('county_map_chatham_county', 'option');
-$durham = get_field('county_map_durham_county', 'option');
-$orange = get_field('county_map_orange_county', 'option');
-$wake = get_field('county_map_wake_county', 'option');
+  $all = get_field('county_map_all_county', 'option');
+  $chatham = get_field('county_map_chatham_county', 'option');
+  $durham = get_field('county_map_durham_county', 'option');
+  $orange = get_field('county_map_orange_county', 'option');
+  $wake = get_field('county_map_wake_county', 'option');
 @endphp
 
 <div class="county-map">
   <div class="container">
+    <div class="js-map">
+      @svg('images/north-carolina-county-map')
+    </div>
     <div class="js-accordion" data-accordion-prefix-classes="my-prefix" data-accordion-multiselectable="none">
       <div class="js-accordion__panel" data-accordion-opened="true">
         <h2 class="js-accordion__header">{{ __('North Carolina', 'sage') }}</h2>
         <div class="js-accordion__panel-wrap js-accordion__panel-wrap--all">
           <div class="percentage">{{ $all['percentage'] }}<sup>%</sup></div>
           <div class="mb-4">{{ $all['text'] }}</div>
-          <div class="amount">$<span class="h1">{{ $all['amount'] }}</span>M</div>
+          <div class="amount">
+            <span aria-hidden="true">$</span>
+            <span class="h1">{!! App\number_format_short($all['amount']) !!}</span>
+            <span class="screen-reader-text"> dollars</span>
+          </div>
+          @if ($all['link'])
+            <a href="{{ $all['link']['url'] }}">{{ $all['link']['title'] }}</a>
+          @endif
         </div>
       </div>
 
@@ -25,7 +34,14 @@ $wake = get_field('county_map_wake_county', 'option');
           <div class="bg-cover" style="background-image: url('<?= $chatham['background_image']['sizes']['medium']; ?>')"></div>
           <div class="percentage">{{ $chatham['percentage'] }}<sup>%</sup></div>
           <div class="mb-4">{{ $chatham['text'] }}</div>
-          <div class="amount">$<span class="h1">{{ $chatham['amount'] }}</span>M</div>
+          <div class="amount">
+            <span aria-hidden="true">$</span>
+            <span class="h1">{!! App\number_format_short($chatham['amount']) !!}</span>
+            <span class="screen-reader-text"> dollars</span>
+          </div>
+          @if ($chatham['link'])
+            <a href="{{ $chatham['link']['url'] }}">{{ $chatham['link']['title'] }}</a>
+          @endif
         </div>
       </div>
 
@@ -35,7 +51,14 @@ $wake = get_field('county_map_wake_county', 'option');
           <div class="bg-cover" style="background-image: url('<?= $durham['background_image']['sizes']['medium']; ?>')"></div>
           <div class="percentage">{{ $durham['percentage'] }}<sup>%</sup></div>
           <div class="mb-4">{{ $durham['text'] }}</div>
-          <div class="amount">$<span class="h1">{{ $durham['amount'] }}</span>M</div>
+          <div class="amount">
+            <span aria-hidden="true">$</span>
+            <span class="h1">{!! App\number_format_short($durham['amount']) !!}</span>
+            <span class="screen-reader-text"> dollars</span>
+          </div>
+          @if ($durham['link'])
+            <a href="{{ $durham['link']['url'] }}">{{ $durham['link']['title'] }}</a>
+          @endif
         </div>
       </div>
 
@@ -45,7 +68,14 @@ $wake = get_field('county_map_wake_county', 'option');
           <div class="bg-cover" style="background-image: url('<?= $orange['background_image']['sizes']['medium']; ?>')"></div>
           <div class="percentage">{{ $orange['percentage'] }}<sup>%</sup></div>
           <div class="mb-4">{{ $orange['text'] }}</div>
-          <div class="amount">$<span class="h1">{{ $orange['amount'] }}</span>M</div>
+          <div class="amount">
+            <span aria-hidden="true">$</span>
+            <span class="h1">{!! App\number_format_short($orange['amount']) !!}</span>
+            <span class="screen-reader-text"> dollars</span>
+          </div>
+          @if ($orange['link'])
+            <a href="{{ $orange['link']['url'] }}">{{ $orange['link']['title'] }}</a>
+          @endif
         </div>
       </div>
 
@@ -55,12 +85,16 @@ $wake = get_field('county_map_wake_county', 'option');
           <div class="bg-cover" style="background-image: url('<?= $wake['background_image']['sizes']['medium']; ?>')"></div>
           <div class="percentage">{{ $wake['percentage'] }}<sup>%</sup></div>
           <div class="mb-4">{{ $wake['text'] }}</div>
-          <div class="amount">$<span class="h1">{{ $wake['amount'] }}</span>M</div>
+          <div class="amount">
+            <span aria-hidden="true">$</span>
+            <span class="h1">{!! App\number_format_short($wake['amount']) !!}</span>
+            <span class="screen-reader-text"> dollars</span>
+          </div>
+          @if ($wake['link'])
+            <a href="{{ $wake['link']['url'] }}">{{ $wake['link']['title'] }}</a>
+          @endif
         </div>
       </div>
-    </div>
-    <div class="js-map">
-      @svg('images/north-carolina-county-map')
     </div>
   </div>
 </div>
