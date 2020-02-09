380e3141864c7c5551eff3f7402407e70ef3a441
Alisa Herr <alisa@unitymakes.us>
2020-02-09 12:52:08 -0500

    Refactor header

--- wp-content/themes/unity-child/resources/views/partials/header-inline.blade.php
+++ wp-content/themes/unity-child/resources/views/partials/header-inline.blade.php
@@ -25,52 +25,49 @@
 @endif
 
 <header class="banner header-inline" role="banner" style="background-color: {{ $header_color }}">
-  <div class="container-wide">
-    <nav class="nav-primary" role="navigation">
-      <div class="navbar" data-text-color="{{ $text_color }}">
-        @php
-          if (!empty($logo_width)) {
-            $custom_logo_width = 'style=width:' . $logo_width . 'px;';
-          } else {
-            $custom_logo_width = '';
-          }
-        @endphp
-        <a class="logo" href="{{ home_url('/') }}" rel="home" {{ $custom_logo_width }}>
-          @if (has_custom_logo())
-            @php
-              $custom_logo_id = get_theme_mod( 'custom_logo' );
-              $logo = wp_get_attachment_image_src( $custom_logo_id , 'simple-logo' );
-              $logo_2x = wp_get_attachment_image_src( $custom_logo_id, 'simple-logo-2x' );
-            @endphp
-            <img src="{{ $logo[0] }}"
-                srcset="{{ $logo[0] }} 1x, {{ $logo_2x[0] }} 2x"
-                alt="{{ get_bloginfo('name', 'display') }}"
-                width="{{ $logo[1] }}" height="{{ $logo[2] }}" />
-          @else
-            {{ get_bloginfo('name', 'display') }}
-          @endif
-        </a>
-        @if (has_nav_menu('primary_navigation'))
-          <div class="menu-trigger-wrapper hide-on-large-only">
-            <input type="checkbox" name="menu-trigger" id="menu-trigger" value="true" />
-            <label for="menu-trigger"><i class="material-icons" aria-label="Show navigation menu">menu</i></label>
-          </div>
-          <div class="navbar-menu">
-            {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'container' => FALSE, 'menu_class' => 'nav nav__primary flex']) !!}
-            @include('partials/searchform', [
-              'context' => 'header',
-            ])
-            <div class="utility-wrapper">
-              <div class="utility-wrapper__nav">
-                {!! wp_nav_menu(['theme_location' => 'secondary_navigation', 'container' => FALSE, 'menu_class' => 'nav nav__secondary flex']) !!}
-                @if ($cta_link = get_field('header_cta_link', 'options'))
-                  <div class="cta-link">
-                    <a href="{{ $cta_link['url'] }}" class="btn" target="{{ $cta_link['target'] }}">{{ $cta_link['title'] }}</a>
-                  </div>
-                @endif
+    @php
+      if (!empty($logo_width)) {
+        $custom_logo_width = 'style=width:' . $logo_width . 'px;';
+      } else {
+        $custom_logo_width = '';
+      }
+    @endphp
+    <div class="container-wide">
+      <a class="logo" href="{{ home_url('/') }}" rel="home" {{ $custom_logo_width }}>
+        @if (has_custom_logo())
+          @php
+            $custom_logo_id = get_theme_mod( 'custom_logo' );
+            $logo = wp_get_attachment_image_src( $custom_logo_id , 'simple-logo' );
+            $logo_2x = wp_get_attachment_image_src( $custom_logo_id, 'simple-logo-2x' );
+          @endphp
+          <img src="{{ $logo[0] }}"
+              srcset="{{ $logo[0] }} 1x, {{ $logo_2x[0] }} 2x"
+              alt="{{ get_bloginfo('name', 'display') }}"
+              width="{{ $logo[1] }}" height="{{ $logo[2] }}" />
+        @else
+          {{ get_bloginfo('name', 'display') }}
+        @endif
+      </a>
+      <div class="menu-trigger-wrapper hide-on-large-only">
+        <input type="checkbox" name="menu-trigger" id="menu-trigger" value="true" />
+        <label for="menu-trigger"><i class="material-icons" aria-label="Show navigation menu">menu</i></label>
+      </div>
+    </div>
+    <nav class="navbar navbar-menu" role="navigation">
+      <div class="container-wide flex space-between">
+        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'container' => FALSE, 'menu_class' => 'nav nav__primary flex']) !!}
+        @include('partials/searchform', [
+          'context' => 'header',
+        ])
+        <div class="utility-wrapper">
+          <div class="utility-wrapper__nav">
+            {!! wp_nav_menu(['theme_location' => 'secondary_navigation', 'container' => FALSE, 'menu_class' => 'nav nav__secondary flex']) !!}
+            @if ($cta_link = get_field('header_cta_link', 'options'))
+              <div class="cta-link">
+                <a href="{{ $cta_link['url'] }}" class="btn" target="{{ $cta_link['target'] }}">{{ $cta_link['title'] }}</a>
               </div>
-            </div>
-          @endif
+            @endif
+          </div>
         </div>
       </div>
     </nav>
