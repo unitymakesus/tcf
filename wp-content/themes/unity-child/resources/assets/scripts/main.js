4d8ca98d007c7b30f689b5a1f637af67fcbd8687
Andrew Marino <andrew@andrewmarino.net>
2020-02-11 11:49:46 -0500

    include various polyfills for ie #37

--- wp-content/themes/unity-child/resources/assets/scripts/main.js
+++ wp-content/themes/unity-child/resources/assets/scripts/main.js
@@ -1,5 +1,6 @@
-// Import parent JS
-// import '../../../../unity-core/dist/scripts/main.js';
+import 'custom-event-polyfill';
+import 'focus-within-polyfill';
+import 'picturefill';
 
 /** Import autoloaded dependencies */
 import './autoload/*';
@@ -7,18 +8,12 @@ import './autoload/*';
 /** Import local dependencies */
 import Router from './util/Router';
 import common from './routes/common';
-import home from './routes/home';
-import aboutUs from './routes/about';
-import archive from './routes/archive';
 import templateEvents from './routes/templateEvents';
 import whyTheTriangle from './routes/whyTheTriangle';
 
 /** Populate Router instance with DOM routes */
 const routes = new Router({
   common,
-  home,
-  aboutUs,
-  archive,
   templateEvents,
   whyTheTriangle,
 });
