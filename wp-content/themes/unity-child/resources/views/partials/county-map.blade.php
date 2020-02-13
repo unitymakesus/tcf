5c317d4edcc3812a99caaa3f930ef85e9c68f369
Andrew Marino <andrew@andrewmarino.net>
2020-02-13 16:56:45 -0500

    update county map template conditionals and field #40

--- wp-content/themes/unity-child/resources/views/partials/county-map.blade.php
+++ wp-content/themes/unity-child/resources/views/partials/county-map.blade.php
@@ -11,18 +11,24 @@
     <div class="js-map">
       @svg('images/north-carolina-county-map')
     </div>
-    <div class="js-accordion" data-accordion-prefix-classes="my-prefix" data-accordion-multiselectable="none">
+    <div class="js-accordion" data-accordion-prefix-classes="tcf" data-accordion-multiselectable="none">
       <div class="js-accordion__panel" data-accordion-opened="true">
         <h2 class="js-accordion__header">{{ __('North Carolina', 'sage') }}</h2>
         <div class="js-accordion__panel-wrap js-accordion__panel-wrap--all">
-          <div class="percentage">{{ $all['percentage'] }}<sup>%</sup></div>
-          <div class="mb-4">{{ $all['text'] }}</div>
-          <div class="amount">
-            <span aria-hidden="true">$</span>
-            <span class="h1">{!! App\number_format_short($all['amount']) !!}</span>
-            <span class="screen-reader-text"> dollars</span>
-          </div>
-          @if ($all['link'])
+          @if($all['data'])
+            <div class="percentage">{{ $all['data'] }}</div>
+          @endif
+          @if($all['text'])
+            <div class="mb-4">{{ $all['text'] }}</div>
+          @endif
+          @if($all['amount'])
+            <div class="amount">
+              <span aria-hidden="true">$</span>
+              <span class="h1">{!! App\number_format_short($all['amount']) !!}</span>
+              <span class="screen-reader-text"> dollars</span>
+            </div>
+          @endif
+          @if($all['link'])
             <a href="{{ $all['link']['url'] }}" target="_blank">{{ $all['link']['title'] }}</a>
           @endif
         </div>
@@ -32,14 +38,20 @@
         <h2 class="js-accordion__header">{{ __('Chatham County', 'sage') }}</h2>
         <div class="js-accordion__panel-wrap js-accordion__panel-wrap--chatham">
           <div class="bg-cover" style="background-image: url('<?= $chatham['background_image']['sizes']['medium']; ?>')"></div>
-          <div class="percentage">{{ $chatham['percentage'] }}<sup>%</sup></div>
-          <div class="mb-4">{{ $chatham['text'] }}</div>
-          <div class="amount">
-            <span aria-hidden="true">$</span>
-            <span class="h1">{!! App\number_format_short($chatham['amount']) !!}</span>
-            <span class="screen-reader-text"> dollars</span>
-          </div>
-          @if ($chatham['link'])
+          @if($chatham['data'])
+            <div class="percentage">{{ $chatham['data'] }}</div>
+          @endif
+          @if($chatham['text'])
+            <div class="mb-4">{{ $chatham['text'] }}</div>
+          @endif
+          @if($chatham['amount'])
+            <div class="amount">
+              <span aria-hidden="true">$</span>
+              <span class="h1">{!! App\number_format_short($chatham['amount']) !!}</span>
+              <span class="screen-reader-text"> dollars</span>
+            </div>
+          @endif
+          @if($chatham['link'])
             <a href="{{ $chatham['link']['url'] }}" target="_blank">{{ $chatham['link']['title'] }}</a>
           @endif
         </div>
@@ -49,14 +61,20 @@
         <h2 class="js-accordion__header">{{ __('Durham County', 'sage') }}</h2>
         <div class="js-accordion__panel-wrap js-accordion__panel-wrap--durham">
           <div class="bg-cover" style="background-image: url('<?= $durham['background_image']['sizes']['medium']; ?>')"></div>
-          <div class="percentage">{{ $durham['percentage'] }}<sup>%</sup></div>
-          <div class="mb-4">{{ $durham['text'] }}</div>
-          <div class="amount">
-            <span aria-hidden="true">$</span>
-            <span class="h1">{!! App\number_format_short($durham['amount']) !!}</span>
-            <span class="screen-reader-text"> dollars</span>
-          </div>
-          @if ($durham['link'])
+          @if($durham['data'])
+            <div class="percentage">{{ $durham['data'] }}</div>
+          @endif
+          @if($durham['text'])
+            <div class="mb-4">{{ $durham['text'] }}</div>
+          @endif
+          @if($durham['amount'])
+            <div class="amount">
+              <span aria-hidden="true">$</span>
+              <span class="h1">{!! App\number_format_short($durham['amount']) !!}</span>
+              <span class="screen-reader-text"> dollars</span>
+            </div>
+          @endif
+          @if($durham['link'])
             <a href="{{ $durham['link']['url'] }}" target="_blank">{{ $durham['link']['title'] }}</a>
           @endif
         </div>
@@ -66,14 +84,20 @@
         <h2 class="js-accordion__header">{{ __('Orange County', 'sage') }}</h2>
         <div class="js-accordion__panel-wrap js-accordion__panel-wrap--orange">
           <div class="bg-cover" style="background-image: url('<?= $orange['background_image']['sizes']['medium']; ?>')"></div>
-          <div class="percentage">{{ $orange['percentage'] }}<sup>%</sup></div>
-          <div class="mb-4">{{ $orange['text'] }}</div>
-          <div class="amount">
-            <span aria-hidden="true">$</span>
-            <span class="h1">{!! App\number_format_short($orange['amount']) !!}</span>
-            <span class="screen-reader-text"> dollars</span>
-          </div>
-          @if ($orange['link'])
+          @if($orange['data'])
+            <div class="percentage">{{ $orange['data'] }}</div>
+          @endif
+          @if($orange['text'])
+            <div class="mb-4">{{ $orange['text'] }}</div>
+          @endif
+          @if($orange['amount'])
+            <div class="amount">
+              <span aria-hidden="true">$</span>
+              <span class="h1">{!! App\number_format_short($orange['amount']) !!}</span>
+              <span class="screen-reader-text"> dollars</span>
+            </div>
+          @endif
+          @if($orange['link'])
             <a href="{{ $orange['link']['url'] }}" target="_blank">{{ $orange['link']['title'] }}</a>
           @endif
         </div>
@@ -83,14 +107,20 @@
         <h2 class="js-accordion__header">{{ __('Wake County', 'sage') }}</h2>
         <div class="js-accordion__panel-wrap js-accordion__panel-wrap--wake">
           <div class="bg-cover" style="background-image: url('<?= $wake['background_image']['sizes']['medium']; ?>')"></div>
-          <div class="percentage">{{ $wake['percentage'] }}<sup>%</sup></div>
-          <div class="mb-4">{{ $wake['text'] }}</div>
-          <div class="amount">
-            <span aria-hidden="true">$</span>
-            <span class="h1">{!! App\number_format_short($wake['amount']) !!}</span>
-            <span class="screen-reader-text"> dollars</span>
-          </div>
-          @if ($wake['link'])
+          @if($wake['data'])
+            <div class="percentage">{{ $wake['data'] }}</div>
+          @endif
+          @if($wake['text'])
+            <div class="mb-4">{{ $wake['text'] }}</div>
+          @endif
+          @if($wake['amount'])
+            <div class="amount">
+              <span aria-hidden="true">$</span>
+              <span class="h1">{!! App\number_format_short($wake['amount']) !!}</span>
+              <span class="screen-reader-text"> dollars</span>
+            </div>
+          @endif
+          @if($wake['link'])
             <a href="{{ $wake['link']['url'] }}" target="_blank">{{ $wake['link']['title'] }}</a>
           @endif
         </div>
